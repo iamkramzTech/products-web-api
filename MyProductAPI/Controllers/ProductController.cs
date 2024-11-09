4e4af339049bfc9bd257bfc22d0fb7c75ddc809b
iamKramzTech <bernaldezm88@yahoo.com>
2024-11-09 20:47:06 +0800

    initial commit

---
+++ MyProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProductAPI.Models;
+using System.Collections.Generic;
+
+namespace MyProductAPI.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/Products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<Product> Products = new List<Product>
+        {
+            new Product{Id=1,Name="Black Jeans",Price=529.75m},
+            new Product{Id=2,Name="White Polo Shirt",Price=255.50m},
+            new Product{Id=3,Name="Air Jordan Shoes",Price=152.25m}
+        };
+
+
+
+        // GET: api/products
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return await Task.FromResult(Ok(Products));
+        }
+
+        // GET: api/products/{id}
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProductByID(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return await Task.FromResult(NotFound(new { Message = $"Product with ID {id} not found!" }));
+
+            }
+            return await Task.FromResult(Ok(product));
+        }
+
+        // POST: api/products
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddNewProduct([FromBody] Product newProduct)
+        {
+            if (newProduct == null)
+            {
+                return await Task.FromResult(BadRequest(new { Message = "Invalid Product data" }));
+                //return BadRequest();
+            }
+
+            // Generate a new ID by finding the max ID and adding 1
+            var newId = Products.Any() ? Products.Max(p => p.Id) + 1 : 1;
+            newProduct.Id = newId;
+
+            Products.Add(newProduct);
+
+            return await Task.FromResult(CreatedAtAction(nameof(GetProductByID), new { id = newProduct.Id }, newProduct));
+        }
+    }
+}
---
+++ MyProductAPI/Program.cs
@@ -0,0 +1,47 @@
+
+namespace MyProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("corsapp", policy =>
+                {
+                    policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseCors("corsapp");
+
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            //app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
